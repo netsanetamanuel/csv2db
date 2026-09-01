d5696db824ff587ccaf7c7b78e2ce0d03160a30e
netsanetamanuel <abrehotamanuell@gmail.com>
2026-09-01 08:23:30 -0500

    chore: initial project setup

---
+++ src/test/java/com/devnet/csv2db/Csv2dbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnet.csv2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csv2dbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
